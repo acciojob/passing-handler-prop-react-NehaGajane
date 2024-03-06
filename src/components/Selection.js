441163d417aa6988a680688bab482db8fea8e01d
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-03-06 20:41:04 +0530

    Added Solution

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, {useState} from 'react';
 
+const Selection = ({applyColor}) => {
+    const [bgColor, setbgColor] = useState("");
+
+    const changeColor = () => {
+        applyColor((background) => {
+            setbgColor(background);
+        });
+    }
+
+    return (
+        <div style={{backgroundColor: bgColor.background, width: 200, height: 200, border: "1px solid gray"}} onClick={changeColor}>
+            <p>Selection</p>
+        </div>
+    )
+}
